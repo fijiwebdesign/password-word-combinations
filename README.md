3d3966c9479a2470110b1f2365ca02318044313b
Gabe Lalasava <gabe@fijiwebdesign.com>
2018-03-17 12:37:08 +0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# password-word-combinations
+# Password word combinations
+
 Combine a list of words into all combinations possible for password generation or cracking
+
+Takes a list of words, prefixes, suffixes and separators and combines them into every permutation. 
+
+Can be used for the wordlist for John the Ripper or phrasen|drescher. 
+
+This is only feasible for a short list of words, prefixes, suffixes and word separators. 
+
+
+## Install locally
+
+```
+git clone https://github.com/fijiwebdesign/password-word-combinations.git
+
+npm install 
+
+node . /path/to/words.txt
+```
+
+## Install globally
+
+```
+npm install -g password-word-combinations
+
+password-word-combinations /path/to/words.txt
+```
+
+Words must be listed one per line. 
+
+`words-combined.txt` will be generated with output words, one per line.
+
+## Usage
+
+```
+
+Word combinations
+
+Generate all word combinations from words, prefixes, suffixes and separators
+
+    Usage:
+
+      Installed globally
+
+        password-word-combinations /path/to/words.txt 
+
+      or installed locally
+
+        npm start -- /path/to/words.txt 
+
+      or 
+
+        node index.js /path/to/words.txt
+
+      or 
+
+        node . /path/to/words.txt
+
+    Options:
+
+      Prefixes
+      --prefix /path/to/prefixes.txt 
+
+      Suffixes
+      --suffix /path/to/suffixes.txt 
+
+      Separators
+      --sep /path/to/separators.txt
+
+      Max words combined
+      --max 3
+
+    Examples:
+
+      node . ../words.txt
+
+      node . ../words.txt --prefix ../prefixes.txt --suffix ../suffixes.txt --sep ../separators.txt
+
+
+```
+
+## Usage with phrasen|drescher private key cracking
+
+Install phrasen|drescher by following: 
+http://leidecker.info/projects/phrasendrescher/index.shtml
+
+Use `password-word-combinations` to generate all word permutations as documented above. 
+
+Run `pd` for `pkey` private key cracking and pass in the `combined-words.txt` via the `-d` option.
+
+
+### Example 
+
+```
+pd pkey -K ~/.ssh/keys/codementor-bali -v -d ./words-combined.txt
+```
+
+
+## Usage with John the ripper
+
+Install Johntheripper
+https://gist.github.com/fijiwebdesign/0a56ec28f1f23412fc16cca70f48d8e0
+
+Todo. Document usage.
+
+## Copyright
+
+ISC - (c) gabe@fijiwebdesign.com 
---
+++ index.js
@@ -0,0 +1,143 @@
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs')
+const os = require('os')
+
+const paths = {
+  words: argv._[0],
+  prefix: argv.prefix,
+  suffix: argv.suffix,
+  sep: argv.sep
+}
+const max = argv.max || 3
+const out_file = argv.out || './words-combined.txt'
+
+let words = {}, out_words = []
+
+if (!paths.words) {
+  console.error('Word file path parameter not set - At least one parameter required')
+  return usage()
+}
+
+Object.keys(paths).forEach(key => {
+  const path = paths[key]
+  if (path) {
+    words[key] = fs.readFileSync(path).toString().split("\n")
+      .filter(word => word) // non empty
+      .filter( (word, index, self) => self.indexOf(word) === index) // unique
+  }
+})
+
+console.log('settings', words)
+
+function prefixer(words, prefixes) {
+  let prefixed = []
+  prefixes.forEach(prefix => words.forEach(word => prefixed.push(prefix + word)))
+  return prefixed
+}
+
+function suffixer(words, suffixes) {
+  let suffixed = []
+  suffixes.forEach(suffix => words.forEach(word => suffixed.push(word + suffix)))
+  return suffixed
+}
+
+function capitalizer(words) {
+  return words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+}
+
+function combiner(words, seperators) {
+  let combined = []
+  words.forEach(word => words.forEach(word2 => {
+    combined.push(word + word2)
+    seperators.forEach(sep => combined.push(word + sep + word2))
+  }))
+  return combined
+}
+
+// words
+const uppercased = capitalizer(words.words)
+out_words = [...out_words, ...words.words, ...uppercased]
+console.log('words', words.words, uppercased)
+
+// prefixed
+const prefixed = prefixer(words.words,  words.prefix)
+const prefixed_upper = prefixer(capitalizer(words.words),  words.prefix)
+out_words = [...out_words, ...prefixed, ...prefixed_upper]
+console.log('prefixed', prefixed, prefixed_upper)
+
+// suffixed
+const suffixed = suffixer(words.words,  words.suffix)
+const suffixed_upper = suffixer(capitalizer(words.words),  words.suffix)
+out_words = [...out_words, ...suffixed, ...suffixed_upper]
+console.log('suffixed', suffixed, suffixed_upper)
+
+// prefixed suffixed
+const prefixed_suffixed = prefixer(suffixed,  words.prefix)
+const prefixed_suffixed_upper = prefixer(capitalizer(suffixed),  words.prefix)
+out_words = [...out_words, ...prefixed_suffixed, ...prefixed_suffixed_upper]
+console.log('prefixed', prefixed_suffixed, prefixed_suffixed_upper)
+
+// combined
+const combined = combiner(words.words, words.sep)
+const combined_upper = combiner(capitalizer(words.words), words.sep)
+out_words = [...out_words, ...combined, ...combined_upper]
+console.log('combined', combined, combined_upper)
+
+// combined prefixed
+const combined_prefixed = prefixer(combined, words.prefix)
+const combined_prefixed_upper = prefixer(capitalizer(combined), words.prefix)
+out_words = [...out_words, ...combined_prefixed, ...combined_prefixed_upper]
+console.log('combined_prefixed', combined_prefixed, combined_prefixed_upper)
+
+// combined suffixed
+const combined_suffixed = suffixer(combined, words.prefix)
+const combined_suffixed_upper = suffixer(capitalizer(combined), words.prefix)
+out_words = [...out_words, ...combined_suffixed, ...combined_suffixed_upper]
+console.log('combined_suffixed', combined_suffixed, combined_suffixed_upper)
+
+// combined prefixed suffixed
+const combined_prefixed_suffixed = prefixer(combined_suffixed, words.prefix)
+const combined_prefixed_suffixed_upper = prefixer(capitalizer(combined_suffixed), words.prefix)
+out_words = [...out_words, ...combined_prefixed_suffixed, ...combined_prefixed_suffixed_upper]
+console.log('combined_prefixed_suffixed', combined_prefixed_suffixed, combined_prefixed_suffixed_upper)
+
+fs.writeFileSync(out_file, out_words.join(os.EOL))
+
+
+/**
+ * usage instructions
+ */
+function usage() {
+  console.log(`
+    Word combinations - Generate all word combinations from words, prefixes, suffixes and separators
+
+    Usage:
+
+      npm start -- /path/to/words.txt 
+
+      or 
+
+      node index.js /path/to/words.txt
+
+    Options:
+
+      Prefixes
+      --prefix /path/to/prefixes.txt 
+
+      Suffixes
+      --suffix /path/to/suffixes.txt 
+
+      Separators
+      --sep /path/to/separators.txt
+
+      Max words combined
+      --max 3
+
+    Examples:
+
+      node . ../words.txt
+
+      node . ../words.txt --prefix ../prefixes.txt --suffix ../suffixes.txt --sep ../separators.txt
+
+  `)
+}
